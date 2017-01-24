5c50bd7fb7ed3a37124f91808b353072dc112ea9
Vinay Rao D <vinayraod@gmail.com>
2017-01-24 14:52:26 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'digTableExtractor',
+    'description': 'digTableExtractor',
+    'author': 'Vinay Rao Dandin',
+    'url': 'https://github.com/usc-isi-i2/dig-table-extractor',
+    'download_url': 'https://github.com/usc-isi-i2/dig-table-extractor',
+    'author_email': 'vrdandin@isi.edu',
+    'version': '0.1.0',
+    'install_requires': ['digExtractor>=0.3.7'],
+    # these are the subdirs of the current directory that we care about
+    'packages': ['digTableExtractor'],
+    'scripts': [],
+}
+
+setup(**config)
