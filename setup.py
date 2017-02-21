7c5085db55f7b07788eea5caa4bfa77e51c9a811
Vinay Rao D <vinayraod@gmail.com>
2017-02-21 14:45:36 -0800

    Changed version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'url': 'https://github.com/usc-isi-i2/dig-table-extractor',
     'download_url': 'https://github.com/usc-isi-i2/dig-table-extractor',
     'author_email': 'vrdandin@isi.edu',
-    'version': '0.2.0',
+    'version': '0.2.1',
     'install_requires': ['digExtractor>=0.3.7'],
     # these are the subdirs of the current directory that we care about
     'packages': ['digTableExtractor'],
